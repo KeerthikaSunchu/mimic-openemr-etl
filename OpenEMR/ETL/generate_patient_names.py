56d25b4e93112d8945feaeeb681b26ca10177fb0
Mohith Surya Kiran Kasula <124705564+mskkasula@users.noreply.github.com>
2024-03-02 13:32:33 -0500

    Create generate_patient_names.py

---
+++ OpenEMR/ETL/generate_patient_names.py
@@ -0,0 +1,41 @@
+!pip install faker
+from faker import Faker
+import mysql.connector
+
+# Set up Faker and the MySQL connection
+fake = Faker()
+conn = mysql.connector.connect(user='omop', password='checkTest@123',
+                              host='localhost',
+                              database='openemr')
+cursor = conn.cursor()
+
+# SQL to select the patient's gender
+select_sql = "SELECT pid, sex FROM patient_data"
+
+# SQL to update the patient's name
+update_sql = "UPDATE patient_data SET fname=%s, lname=%s WHERE pid=%s"
+
+cursor.execute(select_sql)
+patients = cursor.fetchall()
+
+# For each patient, generate a random name based on gender and update the record
+for pid, sex in patients:
+    # Determine the gender and generate a name
+    if sex == 'Male':
+        fname = fake.first_name_male()
+        lname = fake.last_name_male()
+    elif sex == 'Female':
+        fname = fake.first_name_female()
+        lname = fake.last_name_female()
+    else:
+        # If gender is unknown or other, randomly choose
+        fname = fake.first_name()
+        lname = fake.last_name()
+    
+    # Execute the update statement
+    cursor.execute(update_sql, (fname, lname, pid))
+
+# Commit changes and close the connection
+conn.commit()
+cursor.close()
+conn.close()
